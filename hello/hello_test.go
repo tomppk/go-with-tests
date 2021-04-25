1606d5f76a19faab36597904df9345b4da6103c8
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-25 10:13:50 +0300

    create hello name working with tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,11 +2,29 @@ package main
 
 import "testing"
 
+// TestHello with subtests describing different scenarios
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// Helper function to display error message
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 }
